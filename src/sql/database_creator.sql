c1db2e458ccc208f18393b3810d8cf3764293fe9
Julien Condamine <julien.condamine@gmail.com>
2023-04-01 15:40:50 +0200

    debut

--- src/sql/database_creator.sql
+++ src/sql/database_creator.sql
@@ -1,1 +1,11 @@
-#test
+CREATE DATABASE IF NOT EXISTS GLPIRefacto;
+
+CREATE TABLE IF NOT EXISTS `GLPIRefacto`.Users (
+    idUsers int(2000) AUTO_INCREMENT PRIMARY KEY,
+    nom varchar(30) NOT NULL,
+    prenom varchar(30) NOT NULL,
+    sexe varchar(2) NOT NULL,
+    mail varchar(50) NOT NULL,
+    mdp varchar(50) NOT NULL,
+    birth date NOT NULL
+);
