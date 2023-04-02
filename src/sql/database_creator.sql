fa1cb3eebbfda9bcc533db29b2344a15edb92241
GrenouyRotativeInversee <GrenouyRotativeInversee@users.noreply.github.com>
2023-04-02 21:18:37 +0200

    dev_users branch creation and user creation script.

--- src/sql/database_creator.sql
+++ src/sql/database_creator.sql
@@ -1,11 +1,29 @@
-CREATE DATABASE IF NOT EXISTS GLPIRefacto;
+-- commande pour ne pas avoir à mettre c## devant les noms d'users.
+alter session set "_ORACLE_SCRIPT"=true;
 
-CREATE TABLE IF NOT EXISTS `GLPIRefacto`.Users (
-    idUsers int(2000) AUTO_INCREMENT PRIMARY KEY,
-    nom varchar(30) NOT NULL,
-    prenom varchar(30) NOT NULL,
-    sexe varchar(2) NOT NULL,
-    mail varchar(50) NOT NULL,
-    mdp varchar(50) NOT NULL,
-    birth date NOT NULL
-);
+-- création tablespace où sont stockées les données des sites Cergy et Pau
+CREATE TABLESPACE sites 
+   DATAFILE 'sites_data.dbf' 
+   SIZE 4m;
+
+-- création des sites Cergy et Pau : émulé par 2 utilisateurs
+CREATE USER Cergy
+    IDENTIFIED BY Cergy
+    DEFAULT TABLESPACE sites;
+
+CREATE USER Pau
+    IDENTIFIED BY Pau
+    DEFAULT TABLESPACE sites;
+
+-- debug : vérifie que les 2 utilisateurs sont bien créés
+SELECT 
+    username,
+    default_tablespace,
+    profile,
+    authentication_type
+FROM 
+    dba_users
+WHERE 
+    account_status = 'OPEN';
+
+
