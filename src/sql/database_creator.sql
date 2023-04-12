137c728f03facf58bcb617941f907be3221cb7c0
Clement <cledupas@gmail.com>
2023-04-12 23:51:03 +0200

    wip

--- src/sql/database_creator.sql
+++ src/sql/database_creator.sql
@@ -1,6 +1,5 @@
 -- commande pour ne pas avoir à mettre c## devant les noms d'users.
 alter session set "_ORACLE_SCRIPT"=true;
-
 -- création tablespace où sont stockées les données des sites Cergy et Pau
 CREATE TABLESPACE stockageCergy
     DATAFILE 'stockageCergy_data.dbf'
@@ -20,13 +19,12 @@ CREATE USER Pau
     DEFAULT TABLESPACE stockagePau;
 
 CREATE ROLE super_admin;
-GRANT CREATE SEQUENCE, CREATE TRIGGER, CREATE PROCEDURE, CREATE SESSION, CREATE VIEW, DROP USER, DROP TABLESPACE, CREATE ANY TABLE TO super_admin;
+GRANT CREATE DATABASE LINK, CREATE SEQUENCE, CREATE TRIGGER, CREATE PROCEDURE, CREATE SESSION, CREATE VIEW, DROP USER, DROP TABLESPACE, CREATE ANY TABLE TO super_admin;
 GRANT super_admin TO Cergy;
 
 CREATE ROLE local_admin;
 GRANT CREATE SEQUENCE, CREATE TRIGGER, CREATE PROCEDURE, CREATE SESSION, CREATE VIEW, CREATE ANY TABLE TO local_admin;
 GRANT local_admin TO Pau;
-
 
 ALTER USER Cergy quota unlimited on stockageCergy;
 ALTER USER Pau quota unlimited on stockagePau;
@@ -41,5 +39,19 @@ FROM
     dba_users
 WHERE 
     account_status = 'OPEN';
+
+CREATE DATABASE LINK Pau_link2
+CONNECT TO Pau IDENTIFIED BY Pau
+USING 'XE';
+
+SELECT * FROM Utilisateurs@Pau_link2;
+SELECT * FROM Utilisateurs;
+
+
+INSERT INTO Utilisateurs@Pau_link2 VALUES (1,'condamine', 'julien', 'cledupas@gmail.com','0658745285', 'user');
+
+
+
+
 
 
--- src/sql/insert_data.sql
+++ src/sql/insert_data.sql
@@ -1,7 +1,7 @@
-INSERT INTO Utilisateurs VALUES (0,'condamine', 'julien', 'cledupas@gmail.com','0658745285', 'user');
-INSERT INTO Utilisateurs VALUES (1,'ledda', 'Alessia', 'aless@gmail.com','0601020304', 'professeur');
-INSERT INTO Utilisateurs VALUES (2,'couchoude', 'Matt', 'bencouchoude@gmail.com','0695186420', 'user');
-INSERT INTO Utilisateurs VALUES (3,'dupas', 'clem', 'clement@gmail.com','0612121212', 'technicien');
+-- INSERT INTO Utilisateurs VALUES (0,'condamine', 'julien', 'cledupas@gmail.com','0658745285', 'user');
+-- INSERT INTO Utilisateurs VALUES (1,'ledda', 'Alessia', 'aless@gmail.com','0601020304', 'professeur');
+-- INSERT INTO Utilisateurs VALUES (2,'couchoude', 'Matt', 'bencouchoude@gmail.com','0695186420', 'user');
+-- INSERT INTO Utilisateurs VALUES (3,'dupas', 'clem', 'clement@gmail.com','0612121212', 'technicien');
 
 INSERT INTO PROJETS VALUES (0,'Projet 1', 'Description du projet 1', (TO_DATE('2005/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss')), 1);
 INSERT INTO PROJETS VALUES (1,'Projet 2', 'Description du projet 2', (TO_DATE('2023/02/10 22:22:22', 'yyyy/mm/dd hh24:mi:ss')), 1);
@@ -14,7 +14,7 @@ INSERT INTO PARTICIPANTS_PROJETS VALUES (4, 2, 0);
 INSERT INTO PARTICIPANTS_PROJETS VALUES (5, 0, 0);
 INSERT INTO PARTICIPANTS_PROJETS VALUES (6, 0, 1);
 
-INSERT INTO LOGICIELS VALUES (4,'Milanote', (TO_DATE('2020/05/03 21:00:00', 'yyyy/mm/dd hh24:mi:ss')), (TO_DATE('2023/06/10 10:00:00', 'yyyy/mm/dd hh24:mi:ss')));
+INSERT INTO LOGICIELS VALUES (0,'Milanote', (TO_DATE('2020/05/03 21:00:00', 'yyyy/mm/dd hh24:mi:ss')), (TO_DATE('2023/06/10 10:00:00', 'yyyy/mm/dd hh24:mi:ss')));
 INSERT INTO LOGICIELS VALUES (1,'PowerPoint', (TO_DATE('2022/01/01 10:00:00', 'yyyy/mm/dd hh24:mi:ss')), (TO_DATE('2023/02/10 10:00:00', 'yyyy/mm/dd hh24:mi:ss')));
 
 INSERT INTO MATERIELS VALUES (0, 'Ordinateur', 'Ordinateur portable', (TO_DATE('2021/10/03 21:00:00', 'yyyy/mm/dd hh24:mi:ss')), (TO_DATE('2023/06/10 10:00:00', 'yyyy/mm/dd hh24:mi:ss')), 'Ordinateur', 'Parfait', 0);
@@ -26,4 +26,4 @@ INSERT INTO MATERIEL_LOGICIELS VALUES (0, 0, 0);
 INSERT INTO MATERIEL_LOGICIELS VALUES (1, 0, 1);
 INSERT INTO MATERIEL_LOGICIELS VALUES (2, 1, 1);
 
-INSERT INTO TICKETS (SUJET, DESCRIPTION, DATE_CREATION, PRIORITE, STATUS, UTILISATEUR_ID, TECHNICIEN_ID) VALUES ('Problème de connexion', 'Impossible de se connecter à la base de données', (TO_DATE('2021/05/03 21:00:00', 'yyyy/mm/dd hh24:mi:ss')), 'Moyenne', 'En cours', 0, 3);
+INSERT INTO TICKETS (ID, SUJET, DESCRIPTION, DATE_CREATION, PRIORITE, STATUS, UTILISATEUR_ID, TECHNICIEN_ID) VALUES (0, 'Problème de connexion', 'Impossible de se connecter à la base de données', (TO_DATE('2021/05/03 21:00:00', 'yyyy/mm/dd hh24:mi:ss')), 'Moyenne', 'En cours', 0, 3);
